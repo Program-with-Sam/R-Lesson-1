8c3ba714efd05e2cc2b642bc67bf24fa494aaedd
Sam <sam@malpass.me.uk>
2020-02-26 15:50:19 +0000

    Comment

--- Lesson 1.R
+++ Lesson 1.R
@@ -8,20 +8,25 @@ vector
 vector<-c()
 vector<-c(vector, 1, 1)
 
+# Creating a matrix with a vector
 mat<-matrix(vector, nrow=2,byrow=TRUE)
 mat
 
+# Creating a matrix by declaring a vector
 mat<-matrix(c(4:15), nrow=3, byrow=TRUE)
 mat
 
+# Creating a matrix with column and row headers
 rownames<-c("row1", "row2", "row3")
 colnames<-c("col1", "col2", "col3")
 
 mat<-matrix(c(1:9),nrow=3, byrow=TRUE, dimnames=list(rownames,colnames)) 
 mat
 
+# Showing covariance matrix
 covmat<-cov(mat)
 covmat
 
+# Showing singular value decomposition
 svdmat<-svd(mat)
 svdmat
